e09fd94a2269dd8b59d66fe9e4d64caa5d85199d
Sebastian Hanss <sebastian@hanss.info>
2018-10-24 23:09:43 +0200

    rcpp_neigh() is now 2.5x faster than the R-Version. The bottleneck was the function rcpp_which(), which I not completely understood. I therefore only commented it out. Please review.

--- src/rcpp_neigh.cpp
+++ src/rcpp_neigh.cpp
@@ -1,117 +1,140 @@
-#include <Rcpp.h>
-
+#include "rcpp_neigh.h"
 using namespace Rcpp;
 using namespace std;
-
-NumericMatrix rcpp_which(const NumericMatrix& X, int what) {
-
-  int n_rows = X.nrow();
-
-  NumericVector rows(0);
-  NumericVector cols(0);
-
-  for(int ii = 0; ii < n_rows * n_rows; ii++)
-  {
-    if(X[ii] == what)
-    {
-      rows.push_back(ii % n_rows); // rows.push_back(ii % n_rows + 1);
-      cols.push_back(floor(ii / n_rows)); // cols.push_back(floor(ii / n_rows) + 1);
-    }
-  }
-  NumericMatrix out (rows.size(), 2);
-  out(_,0) = rows; out(_,1)=cols;
-  return out;
-
-}
 
 // [[Rcpp::export]]
 NumericMatrix rcpp_neigh(int nrow,
                          int ncol,
                          NumericMatrix mat,
-                         int cat,
-                         NumericVector no_cat,
+                         int n_categories,
+                         NumericVector cells_per_cat,
                          int neighbourhood,
                          float p_neigh,
                          float p_empty) {
 
-  while (cat >= 0) {
+    std::vector<std::pair<int, int> > cell_index =
+            random_cell_indecies(ncol, nrow, 1);
 
-    int j = 0;
+    for (int cat = n_categories; cat >= 0; cat--)
+    {
+        int n_cells = cells_per_cat(cat);
 
-    int ncat = no_cat(cat);
-    // Rcout << "The value of ncat : " << ncat << "\n";
-    while (j < ncat) {
+        while (n_cells > 0) {
 
-      NumericMatrix s;
+            // NumericMatrix s = rcpp_which(mat(Range(1, nrow),
+            //                                  Range(1, ncol)), 0);
 
-      s = rcpp_which(mat(Range(1, nrow), Range(1, ncol)), 0);
+            //int row_pos = as<int>(Rcpp::sample(s.nrow(), 1));
+            //            int row = s(row_pos, 0) + 1; // why + 1 ?
+            //            int col = s(row_pos, 1) + 1;
+            assert(!cell_index.empty());
+            int col = cell_index.back().first;
+            int row = cell_index.back().second;
 
-      int row_pos = as<int>(Rcpp::sample(s.nrow(), 1));
-      int row = s(row_pos, 0) + 1;
-      int col = s(row_pos, 1) + 1;
+            double adjacent;
 
-      double adjacent;
+            double up_cell = mat(row - 1, col);
+            double left_cell = mat(row, col - 1);
+            double right_cell = mat(row, col + 1);
+            double low_cell = mat(row + 1, col);
 
-      double up_cell = mat(row - 1, col);
-      double left_cell = mat(row, col - 1);
-      double right_cell = mat(row, col + 1);
-      double low_cell = mat(row + 1, col);
+            if (neighbourhood == 8) {
 
-      if (neighbourhood == 8) {
+                int upleft_cell = mat(row - 1, col - 1);
+                int upright_cell = mat(row - 1, col + 1);
+                int lowleft_cell = mat(row + 1, col - 1);
+                int lowright_cell = mat(row + 1, col + 1);
 
-        int upleft_cell = mat(row - 1, col - 1);
-        int upright_cell = mat(row - 1, col + 1);
-        int lowleft_cell = mat(row + 1, col - 1);
-        int lowright_cell = mat(row + 1, col + 1);
+                adjacent = sum(NumericVector::create(up_cell,
+                                                     left_cell,
+                                                     right_cell,
+                                                     low_cell,
+                                                     upleft_cell,
+                                                     upright_cell,
+                                                     lowleft_cell,
+                                                     lowright_cell));
+            } else {
 
-        adjacent = sum(NumericVector::create(up_cell,
-                                             left_cell,
-                                             right_cell,
-                                             low_cell,
-                                             upleft_cell,
-                                             upright_cell,
-                                             lowleft_cell,
-                                             lowright_cell));
-      } else {
+                adjacent = sum(NumericVector::create(up_cell,
+                                                     left_cell,
+                                                     right_cell,
+                                                     low_cell));
+            }
 
-        adjacent = sum(NumericVector::create(up_cell,
-                                             left_cell,
-                                             right_cell,
-                                             low_cell));
-      }
+            if (adjacent > 0) {
 
-      if (adjacent > 0){
+                double rnd_num = Rcpp::as<double>(Rcpp::runif(1));
 
-        double rnd_num = Rcpp::as<double>(Rcpp::runif(1));
+                if (rnd_num < p_neigh) {
+                    mat(row, col) = cat;
+                    // Rcout << "The value of mat(row, col) : " << mat(row, col) << "\n";
+                    n_cells--;
+                    cell_index.pop_back();
+                } else {
+                    std::random_shuffle(cell_index.begin(), cell_index.end());
+                }
 
-        if (rnd_num < p_neigh) {
-          mat(row, col) = cat;
-          // Rcout << "The value of mat(row, col) : " << mat(row, col) << "\n";
-          j++;
+            } else {
+
+                double rnd_num = Rcpp::as<double>(Rcpp::runif(1));
+
+                if (rnd_num < p_empty) {
+                    mat(row, col) = cat;
+                    // Rcout << "The value of mat(row, col) : " << mat(row, col) << "\n";
+                    n_cells--;
+                    cell_index.pop_back();
+                } else {
+                    std::random_shuffle(cell_index.begin(), cell_index.end());
+                }
+
+            }
+
+            mat(0, _)         = mat(nrow, _);
+            mat(nrow + 1, _)  = mat(1, _);
+            mat(_, 0)         = mat(_, ncol);
+            mat(_, ncol + 1)  = mat(_,1);
         }
+    }
 
-      } else {
-
-        double rnd_num = Rcpp::as<double>(Rcpp::runif(1));
-
-        if (rnd_num < p_empty) {
-          mat(row, col) = cat;
-          // Rcout << "The value of mat(row, col) : " << mat(row, col) << "\n";
-          j++;
-        }
-
-      }
-
-      mat(0, _)         = mat(nrow, _);
-      mat(nrow + 1, _)  = mat(1, _);
-      mat(_, 0)         = mat(_, ncol);
-      mat(_, ncol + 1)  = mat(_,1);
-
-    } // close while j
-    cat = cat - 1;
-  }// close while i
-  return mat;
+    return mat;
 }
+
+std::vector<std::pair<int, int> > random_cell_indecies(int ncol, int nrow, int offset)
+{
+    std::vector<std::pair<int, int> > cell_index(nrow * ncol);
+    int cntr = 0;
+    for (int col = 0; col < ncol; col++) {
+        for (int row = 0; row < nrow; row++) {
+            cell_index[cntr].first = col + offset;
+            cell_index[cntr].second = row + offset;
+            cntr++;
+        }
+    }
+    std::random_shuffle(cell_index.begin(), cell_index.end());
+
+    return cell_index;
+}
+
+//NumericMatrix rcpp_which(const NumericMatrix& X, int what) {
+
+//    int n_rows = X.nrow();
+
+//    NumericVector rows(0);
+//    NumericVector cols(0);
+
+//    for(int ii = 0; ii < n_rows * n_rows; ii++)
+//    {
+//        if(X[ii] == what)
+//        {
+//            rows.push_back(ii % n_rows); // rows.push_back(ii % n_rows + 1);
+//            cols.push_back(floor(ii / n_rows)); // cols.push_back(floor(ii / n_rows) + 1);
+//        }
+//    }
+//    NumericMatrix out (rows.size(), 2);
+//    out(_,0) = rows; out(_,1)=cols;
+//    return out;
+
+//}
 
 /*** R
 categories = 15
