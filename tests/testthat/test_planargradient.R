c14a3eba75a7553c0a1990d0b58aa175b601ee71
Sebastian Hanss <sebastian@hanss.info>
2019-11-26 14:41:11 +0100

    :white_check_mark: Modified nlm_planargradient uses the right direction test
    test_that("nlm_planargradient uses the right direction" now fails as it should in bug #65

--- tests/testthat/test_planargradient.R
+++ tests/testthat/test_planargradient.R
@@ -18,10 +18,10 @@ test_that("nlm_planargradient produces the right number of columns", {
 
 test_that("nlm_planargradient uses the right direction", {
   set.seed(1)
-  planar_gradient <- nlm_planargradient(ncol = 100, nrow = 100, direction = 180)
+  planar_gradient <- nlm_planargradient(ncol = 80, nrow = 100, direction = 180)
   expect_equal(raster::as.matrix(planar_gradient)[1,1], 0)
   expect_equal(raster::as.matrix(planar_gradient)[50,50], 0.5, tolerance=0.011)
-  expect_equal(raster::as.matrix(planar_gradient)[100,100], 1)
+  expect_equal(raster::as.matrix(planar_gradient)[100,80], 1)
 })
 
 # nolint end
