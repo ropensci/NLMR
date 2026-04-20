1626c784b947ea6f8bf04b50e683d9801c237e2d
Jakub Nowosad <tupiszakaczki@gmail.com>
2026-04-20 15:32:34 +0200

    starts detachment from randomfields

--- R/zzz.R
+++ R/zzz.R
@@ -1,43 +1,43 @@
 # Adapted from: https://github.com/geanders/hurricaneexposure/blob/master/R/zzz.R
 
-.pkgenv <- new.env(parent = emptyenv())
+# .pkgenv <- new.env(parent = emptyenv())
 
-.onLoad  <- function(libname, pkgname) {
+# .onLoad  <- function(libname, pkgname) {
 
-  has_data <- all(requireNamespace("RandomFields", quietly = TRUE),
-                  requireNamespace("RandomFieldsUtils", quietly = TRUE))
+#   has_data <- all(requireNamespace("RandomFields", quietly = TRUE),
+#                   requireNamespace("RandomFieldsUtils", quietly = TRUE))
 
-  .pkgenv[["has_data"]] <- has_data
+#   .pkgenv[["has_data"]] <- has_data
 
-}
+# }
 
-.onAttach <- function(libname, pkgname) {
+# .onAttach <- function(libname, pkgname) {
 
-  if (!.pkgenv$has_data) {
+#   if (!.pkgenv$has_data) {
 
-    message <- paste("Some functions in this package require the 'RandomFields'",
-                     "and 'RandomFieldsUtil' packages which are not available on CRAN anymore.",
-                     "To install them, please run 'install.packages('RandomFields',",
-                     "repos = 'https://predictiveecology.r-universe.dev', type = 'source')'.")
+#     message <- paste("Some functions in this package require the 'RandomFields'",
+#                      "and 'RandomFieldsUtil' packages which are not available on CRAN anymore.",
+#                      "To install them, please run 'install.packages('RandomFields',",
+#                      "repos = 'https://predictiveecology.r-universe.dev', type = 'source')'.")
 
-    message <- paste(strwrap(message), collapse = "\n")
+#     message <- paste(strwrap(message), collapse = "\n")
 
-    packageStartupMessage(message)
+#     packageStartupMessage(message)
 
-  }
-}
+#   }
+# }
 
-hasData <- function(has_data = .pkgenv$has_data) {
+# hasData <- function(has_data = .pkgenv$has_data) {
 
-  if (!has_data) {
+#   if (!has_data) {
 
-    message <- paste("This function requires the 'RandomFields' or 'RandomFieldsUtil'",
-                     "packages which are not available on CRAN anymore. To install them,",
-                     "please run 'install.packages('RandomFields',",
-                     "repos = 'https://predictiveecology.r-universe.dev', type = 'source')'.")
+#     message <- paste("This function requires the 'RandomFields' or 'RandomFieldsUtil'",
+#                      "packages which are not available on CRAN anymore. To install them,",
+#                      "please run 'install.packages('RandomFields',",
+#                      "repos = 'https://predictiveecology.r-universe.dev', type = 'source')'.")
 
-    message <- paste(strwrap(message), collapse = "\n")
+#     message <- paste(strwrap(message), collapse = "\n")
 
-    stop(message, call. = FALSE)
-  }
-}
+#     stop(message, call. = FALSE)
+#   }
+# }
